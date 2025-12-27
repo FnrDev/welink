d45b04c345b9ee6ea9daab7e064383c679a8988a
Ahmed <fnr5541@gmail.com>
2025-12-27 08:51:13 +0300

    feat: first setup for messages

--- welink/SceneDelegate.swift
+++ welink/SceneDelegate.swift
@@ -6,8 +6,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     
     // MARK: - Change this to test different storyboards
     let testMode = true  // Set to true to test a specific storyboard
-    let testStoryboard = "AdminDashboard"  // Change this to your storyboard name
-    let testViewControllerID = "AdminProvidersListVC"  // Change this to your VC identifier
+    let testStoryboard = "Messages"  // Change this to your storyboard name
+    let testViewControllerID = "MessagesVC"  // Change this to your VC identifier
     
     // admin dashboard
     // - ProviderDashboard,ProviderDashboardVC
